c743f7e5a8a48559ca9a40c9f1597b21e5dc6619
kev112 <werewaswas@live.com>
2018-12-10 21:19:13 -0800

    1

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+ def categories_attributes=(categories_hash)
+  # raise categories_hash.inspect
+  # {"0"=>{"name"=>"sd"}, "1"=>{"name"=>"97y99"}}
+
+  categories_hash.each do |i, categories_attributes| 
+    category = Category.find_or_create_by(categories_attributes)
+    self.post_categories.build(:category => category)
+  end
+ end
 
 end
